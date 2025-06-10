eaaea7ad7f2432b8c5e110f1592f393c44ed2f53
Birkan <birkanturan35@gmail.com>
2025-06-10 19:46:56 +0300

    GenericRepository, IGenericRepository arayüzünü tamamen implemente edecek şekilde güncellendi.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-G13T04V9\\SQLEXPRESS;Database=EventlyDB;Trusted_Connection=True;");
+        }
+    }
+
+}
---
+++ DataAccessLayer/Concrete/GenericRepository.cs
@@ -0,0 +1,55 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DataAccessLayer.Concrete
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly Context _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(Context context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public void Insert(T entity)
+        {
+            _dbSet.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+            _context.SaveChanges();
+        }
+
+        public List<T> GetList()
+        {
+            return _dbSet.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public List<T> GetListAll(Expression<Func<T, bool>> filter)
+        {
+            return _dbSet.Where(filter).ToList();
+        }
+    }
+}

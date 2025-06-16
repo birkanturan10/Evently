f8175e377c1c2891680f8422f3c8bf32401cc392
Birkan <birkanturan35@gmail.com>
2025-06-16 18:01:09 +0300

    Güncelleme özelliği kaldırıldı, uygulama sadece listeleme, ekleme ve silme işlemleriyle sade ve işlevsel hale getirildi. Proje basit bir şekilde kullanıma hazır.

--- UI/Controllers/EventController.cs
+++ UI/Controllers/EventController.cs
@@ -3,6 +3,7 @@ using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using EntityLayer;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace UI.Controllers
 {
@@ -31,20 +32,6 @@ namespace UI.Controllers
         public IActionResult Create(Event e)
         {
             _eventManager.AddEvent(e);
-            return RedirectToAction("Index");
-        }
-
-        [HttpGet]
-        public IActionResult Edit(int id)
-        {
-            var eventToEdit = _eventManager.GetById(id);
-            return View(eventToEdit);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Event e)
-        {
-            _eventManager.UpdateEvent(e);
             return RedirectToAction("Index");
         }
 
--- UI/obj/Debug/net8.0/UI.AssemblyInfo.cs
+++ UI/obj/Debug/net8.0/UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+300a196117aa00bb7a81611c65561ff943744b64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54f5e161c55b21c3dc845d43f8083d8f5ebc5c83")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

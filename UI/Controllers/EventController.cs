68e529bf0c1c4d943b07e8a8d4b924db433081b1
Birkan <birkanturan35@gmail.com>
2025-06-18 21:51:05 +0300

    Son güncelleme ile birlikte güncelleme butonu, metodu ve sayfası eklendi. Silme, ekleme ve güncelleme yapan bir uygulama haline geldi.

--- UI/Controllers/EventController.cs
+++ UI/Controllers/EventController.cs
@@ -49,5 +49,37 @@ namespace UI.Controllers
             _eventManager.DeleteEvent(eventToDelete);
             return RedirectToAction("Index");
         }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var eventToEdit = _eventManager.GetById(id);
+            if (eventToEdit == null)
+            {
+                return NotFound();
+            }
+            return View(eventToEdit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Event e)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(e);
+            }
+            var existingEvent = _eventManager.GetById(e.Id);
+            if (existingEvent == null)
+            {
+                return NotFound();
+            }
+            existingEvent.Title = e.Title;
+            existingEvent.Description = e.Description;
+            existingEvent.Date = e.Date;
+            existingEvent.Location = e.Location;
+            existingEvent.CategoryId = e.CategoryId;
+            _eventManager.UpdateEvent(existingEvent);
+            return RedirectToAction("Index");
+        }
     }
 }
